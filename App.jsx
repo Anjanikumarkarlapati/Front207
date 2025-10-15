da1be190e0ab9151567ebaf587539b9bb8a24236
Anjanikumarkarlapati <anjanikumarkarlapati@gmail.com>
2025-10-15 15:24:56 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -10,7 +10,7 @@ function PostList() {
     axios
       .get("https://jsonplaceholder.typicode.com/posts")
       .then((response) => {
-        setPosts(response.data); // store data in state
+        setPosts(response.data); 
         setLoading(false);
       })
       .catch((error) => {
@@ -72,10 +72,9 @@ function UserList() {
 function UseEffectEg() {
   const [count, setCount] = useState(0);
 
-  // useEffect runs after every render when "count" changes
   useEffect(() => {
     document.title = `You clicked ${count} times`;
-  }, [count]); // dependency array
+  }, [count]); 
 
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
